4ed31da313216cbac50caaaef039e8df6b9e4a3b
MartinYeung5 <luckylucky2010201@yahoo.com.hk>
2024-09-13 12:23:22 +0800

    20240913build11

--- pallets/poe/src/benchmarking.rs
+++ pallets/poe/src/benchmarking.rs
@@ -3,10 +3,11 @@
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+//use crate::Pallet as Template;
 use frame_benchmarking::v2::*;
 use frame_system::RawOrigin;
 
+use frame_support::{BoundedVec, pallet_prelude::Get};
 #[benchmarks]
 mod benchmarks {
     use super::*;
